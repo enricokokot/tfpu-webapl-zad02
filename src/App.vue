9ea954ccb91cf44f96717751c76b53dbb755d1aa
coco <kokotenrico@gmail.com>
2022-03-23 17:23:23 +0100

    Setup Components, Enable Fetch Methods

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,41 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld />
+      <v-text-field label="Name" @change="getAll($event)" />
+      <v-data-table />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld,
-  },
 
   data: () => ({
     //
   }),
+
+  methods: {
+    getAge(value) {
+      fetch("https://api.agify.io?name=" + value)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    getGender(value) {
+      fetch("https://api.genderize.io?name=" + value)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    getNationality(value) {
+      fetch("https://api.nationalize.io?name=" + value)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+    getAll(value) {
+      this.getAge(value);
+      this.getGender(value);
+      this.getNationality(value);
+    },
+  },
 };
 </script>
